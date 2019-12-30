edcf83f7c4682f9657363a23d277f5e2b6ec64df
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-30 12:40:30 -0500

    import store

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-// import configureStore from './store/store'
+import configureStore from './store/store'
+
+window.store = configureStore;
 
 ReactDOM.render(
       <App />, document.getElementById('content')
