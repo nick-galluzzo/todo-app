1db4deb43a9c19ac81b273ff8460b185bba88d46
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-30 15:30:04 -0500

    restructured root entrance

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { receiveTodo } from './actions/todo_actions';
+
 import Root from './components/root';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
 
-window.store = configureStore;
-
-ReactDOM.render(
-      <Root />, document.getElementById('content')
-  );
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
