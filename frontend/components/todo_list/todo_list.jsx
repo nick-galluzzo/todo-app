3596a8bf867589daec71d22506458e699c5cefa7
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-30 17:44:13 -0500

    bundle

--- frontend/components/todo_list/todo_list.jsx
+++ frontend/components/todo_list/todo_list.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+// Components
+import TodoListItem from './todo_list_item';
 
-export default class Todolist extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+
+class TodoList extends React.Component {
 
   render() {
-    return (
-      <div>Hi</div>
+    const { todos, receiveTodo } = this.props;
+    const todoItems = todos.map(todo => (
+        <TodoListItem
+          key={`todo-list-item${todo.id}`}
+          todo={todo}
+          receiveTodo={ receiveTodo } />
+      )
+    );
+
+    return(
+      <div>
+        <ul className="todo-list">
+          { todoItems }
+        </ul>
+      </div>
     );
   }
 }
+
+export default TodoList;
