1744a5aeaa7bc8962c6e113de844e20d88542255
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-31 12:00:32 -0500

    add removeTodo function

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -1,5 +1,6 @@
 export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
 
 export const receiveTodos = (todos) => ({
   type: RECEIVE_TODOS,
@@ -8,6 +9,11 @@ export const receiveTodos = (todos) => ({
 
 export const receiveTodo = (todo) => ({
   type: RECEIVE_TODO,
+  payload: todo
+})
+
+export const removeTodo = (todo) => ({
+  type: REMOVE_TODO,
   payload: todo
 })
 
